73839867dec7186e396291eb5ad3ae7fd98222d7
Rhythm Jain <rhythmalive@gmail.com>
2014-06-13 22:19:34 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c63311905d12df9c462b1e999c00c94d586c8ce3d76d6e0d96f7be4ba51eab42d07788ca639b67bca25b72c79339e0588bb4a12d660a33c1d22f13c8a362dec5'
